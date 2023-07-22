77737ba2c15a16054f175c209bc25548f3e76a45
Jasper <jasperleejk@gmail.com>
2023-07-22 13:48:53 +0800

    first commit

---
+++ server/src/routes/User.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+
+export const userRouter = express.Router();
+
+
+userRouter.get('/', (req, res) => {
+    res.send('Hello World!');
+});
---
+++ server/src/routes/index.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+
+// import sub routers
+import { userRouter } from './User';
+
+
+export const routes = express.Router();
+
+// use sub routers
+routes.use('/user', userRouter);
