09c4b01eb1c35366a4d4a458d3d67ec27c6a86f9
Jasper <jasperleejk@gmail.com>
2023-11-12 00:05:21 +0800

    fix prisma issue after updating sequence table

--- server/src/utils/sequence.ts
+++ server/src/utils/sequence.ts
@@ -3,47 +3,52 @@ import { SEQUENCE_KEYS } from "./constants";
 
 const prisma = new PrismaClient();
 
-export const getNextSequenceValue = async (company_id: number, sequenceName: string) => {
-    const results = await prisma.$transaction([
-        prisma.sequences.findFirst({
-            where: {
-                company_id: company_id,
-                sequence_key: sequenceName
-            }
-        }),
-        prisma.sequences.update({
-            where: {
-                company_id: company_id,
-                sequence_key: sequenceName
-            },
-            data: {
-                sequence_value: {
-                    increment: 1
-                }
-            }
-        })
-    ]);
+export const getNextSequenceValue = async (
+  company_id: number,
+  sequenceName: string
+) => {
+  const results = await prisma.$transaction([
+    prisma.sequences.findFirst({
+      where: {
+        company_id: company_id,
+        sequence_key: sequenceName,
+      },
+    }),
+    prisma.sequences.update({
+      where: {
+        company_id_sequence_key: {
+          company_id: company_id,
+          sequence_key: sequenceName,
+        },
+      },
+      data: {
+        sequence_value: {
+          increment: 1,
+        },
+      },
+    }),
+  ]);
 
-    return results[0]?.sequence_value || 1;
-}
+  return results[0]?.sequence_value || 1;
+};
 
 export const instantiateSequences = async (company_id: number) => {
-    for (const [key, val] of Object.entries(SEQUENCE_KEYS)) {
-        const sequence = await prisma.sequences.findFirst({
-            where: {
-                company_id: company_id,
-                sequence_key: key
-            }
-        });
+  for (const [key, val] of Object.entries(SEQUENCE_KEYS)) {
+    const sequence = await prisma.sequences.findFirst({
+      where: {
+        company_id: company_id,
+        sequence_key: key,
+      },
+    });
 
-        if (!sequence) {
-            await prisma.sequences.create({
-                data: {
-                    company_id: company_id,
-                    sequence_key: val,
-                    sequence_value: 1
-                }
-            });
-        }
+    if (!sequence) {
+      await prisma.sequences.create({
+        data: {
+          company_id: company_id,
+          sequence_key: val,
+          sequence_value: 1,
+        },
+      });
     }
-}
+  }
+};
