d4730aa9edb3b889f2019367fb98a533a87f4bbe
Chua Zun Sheng <chuazunsheng26@gmail.com>
2023-11-11 23:15:48 +0800

    view roster have profile img

--- client/src/modules/schedule/manager/view-schedule.tsx
+++ client/src/modules/schedule/manager/view-schedule.tsx
@@ -21,16 +21,18 @@ const ViewSchedule = () => {
   const roster = location.state?.roster as IRoster[];
   const type = location.state?.type as string;
   const startDate = moment(new Date(roster[0].start_date));
-  const [rosterToDelete, setRosterToDelete] = React.useState<IRoster | null>(null);
+  const [rosterToDelete, setRosterToDelete] = React.useState<IRoster | null>(
+    null
+  );
   const [submitLoading, setSubmitLoading] = React.useState(false);
   useEffect(() => {
-    if(submitLoading && rosterToDelete){
+    if (submitLoading && rosterToDelete) {
       deleteRoster(rosterToDelete).finally(() => {
         toast.success("Roster delete successfully");
         navigate(`/${PATHS.SCHEDULE}`, { replace: true });
-        setRosterToDelete(null)
-        setSubmitLoading(false)
-      })
+        setRosterToDelete(null);
+        setSubmitLoading(false);
+      });
     }
   }, [submitLoading]);
 
@@ -50,40 +52,65 @@ const ViewSchedule = () => {
           <Label htmlFor="schedule-employees" value="Scheduled Employees" />
           <div id="schedule-employees">
             {roster.map((rosteridx, idx) => (
-              <div className = "mt-2">
+              <div className="mt-2">
                 <div className="flex">
-                <p style={{ marginRight: '10px' }}>Created by: {rosteridx.created_by}</p>
-                <p style={{ marginRight: '10px' }}>Start Date: {new Date(rosteridx.start_date).toLocaleDateString()}</p>
-                <p>End Date: {new Date(rosteridx.end_date).toLocaleDateString()}</p>
+                  <p style={{ marginRight: "10px" }}>
+                    Created by: {rosteridx.created_by}
+                  </p>
+                  <p style={{ marginRight: "10px" }}>
+                    Start Date:{" "}
+                    {new Date(rosteridx.start_date).toLocaleDateString()}
+                  </p>
+                  <p>
+                    End Date:{" "}
+                    {new Date(rosteridx.end_date).toLocaleDateString()}
+                  </p>
                 </div>
-              <div className="border border-solid border-black p-2 mt-2">
-                <p>{rosteridx.type}</p>
-                <div key={idx} className=" grid grid-cols-5 gap-4">
-                  {rosteridx.schedules?.map((schedule, scheduleIdx) => (
-                    <div key={scheduleIdx}>
-                      <p>{capitalizeString(schedule.user?.fullname || "")}</p>
-                      <p>{capitalizeString(schedule.user?.position || "")}</p>
-                      <p>{schedule.shift} Shift</p>
-                      <p>{schedule.user?.contact_no}</p>
+                <div className="border border-solid border-black p-2 mt-2">
+                  <p>{rosteridx.type}</p>
+                  <div key={idx} className=" grid grid-cols-5 gap-4">
+                    {rosteridx.schedules?.map((schedule, scheduleIdx) => (
+                      <div key={scheduleIdx}>
+                        <Avatar
+                          size="sm"
+                          img={schedule.user?.profile_image}
+                          style={{ display: "inline-block", margin: "0" }}
+                        ></Avatar>
+                        <p>{capitalizeString(schedule.user?.fullname || "")}</p>
+                        <p>{capitalizeString(schedule.user?.position || "")}</p>
+                        <p>{schedule.shift} Shift</p>
+                        <p>{schedule.user?.contact_no}</p>
+                      </div>
+                    ))}
+                  </div>
+                  {startDate.isSameOrAfter(moment(), "day") && (
+                    <div
+                      className="mt-4 flex"
+                      style={{ justifyContent: "flex-end" }}
+                    >
+                      <EditButton
+                        size="sm"
+                        style={{ marginRight: "10px" }}
+                        onClick={() => {
+                          navigate(
+                            `/${PATHS.SCHEDULE}/${PATHS.EDIT_SCHEDULE}`,
+                            {
+                              state: { rosteridx },
+                              replace: true,
+                            }
+                          );
+                        }}
+                      />
+                      <DeleteButton
+                        size="sm"
+                        onClick={() => {
+                          setRosterToDelete(rosteridx);
+                          setSubmitLoading(true);
+                        }}
+                      />
                     </div>
-                  ))}
+                  )}
                 </div>
-                {startDate.isSameOrAfter(moment(), 'day') && (
-                  <div className="mt-4 flex" style={{ justifyContent: "flex-end" }}>
-                  <EditButton size="sm" style={{ marginRight: "10px" }} onClick={() => {
-                    navigate(`/${PATHS.SCHEDULE}/${PATHS.EDIT_SCHEDULE}`, {
-                      state: { rosteridx },
-                      replace: true
-                    });
-                  }}/>
-                  <DeleteButton size="sm"
-                  onClick={() => {
-                    setRosterToDelete(rosteridx);
-                    setSubmitLoading(true);
-                    }} />
-                </div>
-                )}
-              </div>
               </div>
             ))}
             {/* {schedule.employeesSelected.map((employee, idx) => (
